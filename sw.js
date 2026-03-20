fdc6b987bc31ae975d8f8aceafe127ac81ee6ee0
Kyunus175 <164312910+Kyunus175@users.noreply.github.com>
2026-03-20 15:07:53 +0530

    Update sw.js
    New update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// SabrTime Service Worker v2.0
-const CACHE_NAME = 'sabrtime-v2';
+// SabrTime Service Worker v3.0
+const CACHE_NAME = 'sabrtime-v3';
 const URLS_TO_CACHE = [
   '/sabrtime/',
   '/sabrtime/index.html',
