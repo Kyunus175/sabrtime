81aa8ac620fc997799ab4599ec1818e92da73fa3
Kyunus175 <164312910+Kyunus175@users.noreply.github.com>
2026-06-29 16:49:57 +0530

    Update Worker.js
    Updated

--- Worker.js
+++ Worker.js
@@ -1,7 +1,14 @@
-addEventListener('fetch',e=>{e.respondWith(handle(e.request))})
+addEventListener('fetch',e=>{
+e.respondWith(handle(e.request))
+})
 async function handle(r){
 const b=await r.json()
-const res=await fetch('https://api.minepi.com/v2/payments/'+b.paymentId+'/approve',{method:'POST',headers:{'Authorization':'key '+PI_API_KEY}})
+const res=await fetch(
+'https://api.minepi.com/v2/payments/'+b.paymentId+'/approve',
+{method:'POST',headers:{'Authorization':'key '+PI_API_KEY}}
+)
 const d=await res.json()
-return new Response(JSON.stringify(d),{headers:{'Content-Type':'application/json','Access-Control-Allow-Origin':'*'}})
+return new Response(JSON.stringify(d),{
+headers:{'Content-Type':'application/json','Access-Control-Allow-Origin':'*'}
+})
 }
