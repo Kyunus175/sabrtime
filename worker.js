459c92f7a779942e14030a93c8ebe2b200044962
Kyunus175 <164312910+Kyunus175@users.noreply.github.com>
2026-06-29 17:58:24 +0530

    Update worker.js
    Commit

--- worker.js
+++ worker.js
@@ -2,22 +2,10 @@ export default {
   async fetch(request, env) {
     try {
       const b = await request.json()
-      const res = await fetch(
-        'https://api.minepi.com/v2/payments/' + b.paymentId + '/approve',
-        {
-          method: 'POST',
-          headers: { 'Authorization': 'key ' + env.PI_API_KEY }
-        }
-      )
-      const d = await res.json()
-      return new Response(JSON.stringify(d), {
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        }
-      })
+      const res = await fetch('https://api.minepi.com/v2/payments/'+b.paymentId+'/approve',{method:'POST',headers:{'Authorization':'key '+env.PI_API_KEY}})
+      return new Response(await res.text(),{headers:{'Content-Type':'application/json','Access-Control-Allow-Origin':'*'}})
     } catch(e) {
-      return new Response(e.message, { status: 500 })
+      return new Response(e.message,{status:500})
     }
   }
 }
